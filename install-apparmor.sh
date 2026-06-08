933265ce280b529b58ad7f64b00c963806f66504
Stian Kristoffersen <wayphinder@gmail.com>
2026-06-08 18:01:27 +0200

    Install script with and without AppArmor

--- install-apparmor.sh
+++ install-apparmor.sh
@@ -5,6 +5,10 @@ systemctl stop netfoil --quiet
 
 set -e
 
+# AppArmor
+cp packaging/apparmor/netfoil /etc/apparmor.d/netfoil
+apparmor_parser -r /etc/apparmor.d/netfoil
+
 sleep .2
 cp packaging/systemd/netfoil.socket /usr/lib/systemd/system/netfoil.socket
 cp packaging/systemd/netfoil.service /usr/lib/systemd/system/netfoil.service
